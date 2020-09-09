aa163853792a0b799b37c609449352a470112db1
owpk <vzvz4@mail.ru>
2020-09-09 17:11:41 +0900

    upd

--- test/src/main/java/org/owpk/Camera.java
+++ test/src/main/java/org/owpk/Camera.java
@@ -2,9 +2,11 @@ package org.owpk;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class Camera {
 
   private final CameraRoll cameraRoll;
--- test/src/test/groovy/CameraTest.groovy
+++ test/src/test/groovy/CameraTest.groovy
@@ -2,21 +2,21 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import org.owpk.Camera
-import org.owpk.ColoredCameraRoll
+import org.owpk.CameraRoll
 
 class CameraTest {
 
-    def camRoll = Mockito.mock(ColoredCameraRoll.class)
+    def camRoll = Mockito.mock(CameraRoll.class)
     def cam
 
     @Before
     void setup() {
+        Mockito.when(camRoll.printPhoto()).thenReturn('mock')
         cam = new Camera(camRoll)
     }
 
     @Test
-    void 'should return colored roll'() {
-        Mockito.when(camRoll.printPhoto()).thenReturn('mock')
+    void 'should make photo'() {
         assert cam.doPhoto() == "The camera is taking photo: ${camRoll.printPhoto()}"
     }
 }
