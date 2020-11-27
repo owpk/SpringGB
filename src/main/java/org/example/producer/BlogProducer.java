28f3c3283f7de8fc49c9cd28b52e63e93994471b
owpk <vzvz4@mail.ru>
2020-11-27 20:53:03 +0000

    fix

--- src/main/java/org/example/producer/BlogProducer.java
+++ src/main/java/org/example/producer/BlogProducer.java
@@ -1,25 +1,19 @@
 package org.example.producer;
 
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-
-import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class BlogProducer {
-    private static final String EXCHANGE_NAME2 = "news";
+    private static final String[] THEMES = {"java", "js", "php"};
 
     public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME2, BuiltinExchangeType.DIRECT);
-            String message = "info: Hello World!";
-            channel.basicPublish(EXCHANGE_NAME2, "java",
-                    null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Sent '" + message + "'");
+        Producer producer = new Producer("newsIt");
+        Random r = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            String theme = THEMES[r.nextInt(THEMES.length)];
+            producer.produce(theme, BlogFactory.getBlog(theme));
         }
     }
+
+
 }
