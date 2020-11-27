8a45b1d927404672e28b653dd1a838517d670d8b
owpk <vzvz4@mail.ru>
2020-11-27 18:19:32 +0000

    pull

---
+++ src/main/java/org/example/producer/BlogProducer.java
@@ -0,0 +1,25 @@
+package org.example.producer;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class BlogProducer {
+    private static final String EXCHANGE_NAME2 = "news";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME2, BuiltinExchangeType.DIRECT);
+            String message = "info: Hello World!";
+            channel.basicPublish(EXCHANGE_NAME2, "java",
+                    null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
