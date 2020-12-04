f8212a8fec1e4c11481542b2a76b21ac64ec8ad0
owpk-q <vzvz4@mail.ru>
2020-12-04 22:26:52 +0900

    upd

--- src/test/java/com/geekbrains/geek/market/ProductControllerTest.groovy
+++ src/test/java/com/geekbrains/geek/market/ProductControllerTest.groovy
@@ -3,6 +3,7 @@ package com.geekbrains.geek.market
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 import org.springframework.security.test.context.support.WithMockUser
